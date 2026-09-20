add5d33f3bc39426c02b354524673ac236144c5a
Vishu Kumar <vishu.singh.cse.2025@miet.ac.in>
2026-09-20 11:22:52 +0530

    Time: 0 ms (100%), Space: 10.1 MB (8.54%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.c
@@ -0,0 +1,22 @@
+int search(int* nums, int numsSize, int target) {
+    int l=0;
+    int r=numsSize-1;
+    int mid=0;
+    while(l<=r){
+        mid = l+(r-l)/2;
+        if (nums[mid]==target){
+            return mid;
+        }
+        else if (
+            nums[mid]<target
+        ){
+            l=mid+1;
+        }
+        else{
+            r=mid-1;
+        }
+        
+
+    }
+    return -1;
+}
